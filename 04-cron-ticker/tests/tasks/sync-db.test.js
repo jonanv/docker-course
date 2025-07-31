6468c4f9ca5c409f581d98433e932b045f306e36
jonanv <jonan-vargas23@hotmail.com>
2025-07-30 20:48:56 -0500

    :art: (docker-commands): Fix word

--- 04-cron-ticker/tests/tasks/sync-db.test.js
+++ 04-cron-ticker/tests/tasks/sync-db.test.js
@@ -1,7 +1,7 @@
 const { syncDB } = require('../../tasks/sync-db');
 
 describe('Pruebas en Sync-DB', () => {
-    test('Debe ejecuntar el proceso 2 veces', () => {
+    test('Debe ejecutar el proceso 2 veces', () => {
         syncDB();
         const times = syncDB();
         // console.log('Se llamo ', times);
